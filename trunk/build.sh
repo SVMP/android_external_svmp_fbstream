dea108acc67b0dfb351221f1735a1e76eb33435b
David Keppler <dkeppler@mitre.org>
2013-07-19 11:39:29 -0400

    Improve build.sh for automation
    Change-Id: Ifbef703e421342b0e0e99cd9d6c553dfc1ecf73d

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+AOSP_DIR=$SELF_DIR/../../../../
+
+echo "SELF_DIR=$SELF_DIR"
+echo "AOSP_DIR=$AOSP_DIR"
+
+# check prerequisite symlinks
+if [ ! -L $SELF_DIR/third_party/android_tools/aosp ] ; then
+    ln -f -s $AOSP_DIR $SELF_DIR/third_party/android_tools/aosp
+fi
+
+if [ ! -L $SELF_DIR/third_party/android_tools/sdk ] ; then
+    echo "ERROR: fbstream incorrectly configured, missing SDK symlink. See the README."
+    exit 1
+fi
+
+if [ ! -L $SELF_DIR/third_party/android_tools/ndk ] ; then
+    echo "ERROR: fbstream incorrectly configured, missing NDK symlink. See the README."
+    exit 1
+fi
+
+# Do the build
+
+cd $SELF_DIR
 #gclient sync --nohooks
 export PATH=$PATH:../depot_tools
 export WEBRTC_SKIP_RESOURCES_DOWNLOAD=1
